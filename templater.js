0bde63cd37380853eba6f8bc93d20332a98a6fae
Anton Robitnitsky <robitnitsky.av@gmail.com>
2018-05-06 14:20:54 +0300

    stage 3

--- templater.js
+++ templater.js
@@ -1,16 +1,28 @@
 var Templater = {
 	templates: {},
+	element: {},
 	run: function () {
-		for (var tag in this.templates) {
-			for (let i = 0; i < this.findElements(tag).length;) {
-				this.findElements(tag)[i].outerHTML = this.templates[tag];
-			}
-		}
+		this.render(this.templates, this.element);
 	},
 	findElements: function (tag) {
-		return document.getElementsByTagName(tag);
+		this.element[tag] = document.getElementsByTagName(tag)[0];
 	},
 	addTag: function (tag, template) {
 		this.templates[tag] = template;
+		this.findElements(tag);
+	},
+	render: function (templates, element) {
+		var templateItem = this.templates['bootstrap_button'];
+		var elementItem = this.element['bootstrap_button'];
+		templateItem = templateItem.replace(/{{class}}/, elementItem.className);
+		templateItem = templateItem.replace(/{{type}}/, elementItem.getAttribute("type"));
+		if (elementItem.innerHTML.search(/<\w+>/) > -1) {
+			templateItem = templateItem.replace(/{{html}}/, elementItem.innerHTML);
+			elementItem.outerHTML = templateItem
+		} else {
+			templateItem = templateItem.replace(/{{html}}/, '');
+			this.element['bootstrap_button'].outerHTML = templateItem
+			var newBtn = document.getElementsByTagName('button')[0].value = this.element['bootstrap_button'].innerHTML;
+		}
 	}
 }
