d0800d674f2d6838e48e5eb21af3d3fac1e73f3f
Employee <employee@Justs-Mac-mini.local>
2018-06-04 18:59:37 +0300

    add recursion

--- templater.jq.js
+++ templater.jq.js
@@ -1,53 +1,53 @@
 (function ($) {
 
-    let methods = {
-        init (elements, options) {
-            let tags = options.tags;
-            this.output(elements, tags);
-            this.output(elements, tags);
-        },
-        getTemplateParameters (template) {
-            let regExp = /\{\{(\w+?)\}\}/ig;
-            let execResultsArray;
-            let templateParameters = [];
-            while ((execResultsArray = regExp.exec(template)) !== null) {
-                templateParameters.push(execResultsArray[1]);
+
+    function init (elements, options) {
+        const tags = options.tags;
+        output(elements, tags);
+    }
+
+    function getTemplateParameters (template) {
+        const templateParameter = /\{\{(\w+?)\}\}/ig;
+        let execResults;
+        let templateParameters = [];
+        while ((execResults = templateParameter.exec(template)) !== null) {
+            templateParameters.push(execResults[1]);
+        }
+        return templateParameters;
+    }
+
+    function render (templateParameters, renderedTemplate, element) {
+        for (let i = 0; i < templateParameters.length; i++) {
+            let tempStr = '{{' + templateParameters[i] + '}}';
+            if ($(element).attr(templateParameters[i])) {
+                renderedTemplate = renderedTemplate.replace(tempStr, element.getAttribute(templateParameters[i]));
+            } else {
+                renderedTemplate = renderedTemplate.replace(tempStr, element.innerHTML);
             }
-            return templateParameters;
-        },
-        isHTML (str) {
-            let tagReg = /<(.|\n)*?>/;
-            return tagReg.test(str);
-        },
-        render (templateParameters, renderedTemplate, element) {
-            for (let i = 0; i < templateParameters.length; i++) {
-                let tempStr = '{{' + templateParameters[i] + '}}';
-                if (templateParameters[i] !== 'html') {
-                    renderedTemplate = renderedTemplate.replace(tempStr, element.getAttribute(templateParameters[i]));
-                } else if (methods.isHTML(element.innerHTML)) {
-                    renderedTemplate = renderedTemplate.replace(tempStr, element.innerHTML);
-                } else {
-                    renderedTemplate = renderedTemplate.replace(tempStr, element.textContent);
+        }
+        return renderedTemplate;
+    }
+
+    function replaceElement (element, renderedTemplate) {
+        console.log(element)
+        element.outerHTML = renderedTemplate;
+    }
+
+    function output (elements, tags) {
+        for (key in tags) {
+            let elementsHtml = elements.find(key);
+            let templateParameters = getTemplateParameters(tags[key]);
+            elementsHtml.each(function(index, element){
+                if ($(element).find(key).length !== 0) {
+                    output($(element), tags);
                 }
-            }
-            return renderedTemplate;
-        },
-        replace (element, template) {
-            element.outerHTML = template;
-        },
-        output (elements, tags) {
-            for (key in tags) {
-                let elementsHtml = elements.find(key);
-                elementsHtml.each(function(){
-                    let templateParameters = methods.getTemplateParameters(tags[key]);
-                    methods.replace(this, methods.render(templateParameters, tags[key], this));
-                })
-            }
+                replaceElement(element, render(templateParameters, tags[key], element));
+            })
         }
     }
 
     $.fn.templater = function(options) {
-        return methods.init(this, options);
+        return init(this, options);
     };
 
 })(jQuery)
